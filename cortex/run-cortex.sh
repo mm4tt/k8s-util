9fd5b88df4f0e65eb497d7f86253693e8514d986
Mateusz Matejczyk <mmat@google.com>
2019-05-21 17:10:08 +0200

    Stop using deprecated docker --link feature.

--- cortex/run-cortex.sh
+++ cortex/run-cortex.sh
@@ -4,9 +4,11 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 docker stop cortex 2>/dev/null || true
 docker rm cortex 2>/dev/null || true
+docker network create cortex-network 2>/dev/null || true
 
 docker run -d \
   --name=cortex \
+  --network=cortex-network \
   -v $DIR/cortex.yaml:/etc/cortex.yaml:ro \
   -v /mnt/disks/cortex-db:/cortex-db \
   quay.io/cortexproject/cortex:master-88541de7 -config.file=/etc/cortex.yaml
--- cortex/run-nginx.sh
+++ cortex/run-nginx.sh
@@ -4,10 +4,11 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 docker stop nginx 2>/dev/null || true
 docker rm nginx 2>/dev/null || true
+docker network create mynet 2>/dev/null || true
 
 docker run -d \
   --name=nginx \
-  --link cortex:cortex \
+  --network=cortex-network \
   -p 9090:9090 \
   -v $DIR/nginx.conf:/etc/nginx/nginx.conf:ro \
   nginx:1.16.0
