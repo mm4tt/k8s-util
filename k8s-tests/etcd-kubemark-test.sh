8646e0e70718cee3484c1b63b7113383a0e6df6c
Mateusz Matejczyk <mmat@google.com>
2019-07-18 11:59:21 +0200

    Update the etcd test script.

--- k8s-tests/etcd-kubemark-test.sh
+++ k8s-tests/etcd-kubemark-test.sh
@@ -8,15 +8,15 @@ if [ $# -ne 2 ]
     exit 1
 fi
 
-test_name=${1?}
+run_ name=${1?}
 num_nodes=${2?}
 
 perf_test_branch=etcd_tests
-test_infra_commit=63eb09459
+test_infra_commit=0bf601772
 
-log_dir=~/log/etcd-test-kubemark/${test_name}
+log_dir=~/log/etcd-test-kubemark/${run_ name}
 mkdir -p ${log_dir}
-log_file=${log_dir}/log
+log_file=${log_dir}/log_${num_nodes}
 
 log() { echo $1 | ts | tee -a $log_file; }
 
@@ -38,7 +38,7 @@ export ZONE=us-east1-b
 export HEAPSTER_MACHINE_TYPE=n1-standard-32
 export KUBE_DNS_MEMORY_LIMIT=300Mi
 
-export CLUSTER=${test_name}
+export CLUSTER=${run_ name}
 export KUBE_GCE_NETWORK=${CLUSTER}
 export INSTANCE_PREFIX=${CLUSTER}
 export KUBE_GCE_INSTANCE_PREFIX=${CLUSTER}
@@ -55,14 +55,17 @@ go run hack/e2e.go -- \
     --kubemark-nodes=$NUM_NODES \
     --check-version-skew=false \
     --up \
+    --down \
     --test=false \
     --test-cmd=$GOPATH/src/k8s.io/perf-tests/run-e2e.sh \
     --test-cmd-args=cluster-loader2 \
+    --test-cmd-args=--enable-prometheus-server=true \
+    --test-cmd-args=--experimental-gcp-snapshot-prometheus-disk=true \
+    --test-cmd-args=--experimental-prometheus-disk-snapshot-name="${run_ name}_${num_nodes}" \
     --test-cmd-args=--nodes=$NUM_NODES \
     --test-cmd-args=--provider=kubemark \
-    --test-cmd-args=--enable-prometheus-server=true \
-    --test-cmd-args=--tear-down-prometheus-server=false \
     --test-cmd-args=--report-dir=~/log/golang1.13/artifacts \
+    --test-cmd-args=--tear-down-prometheus-server=true \
     --test-cmd-args=--testconfig=$GOPATH/src/k8s.io/perf-tests/clusterloader2/testing/load/config.yaml \
     --test-cmd-args=--testoverrides=./testing/load/kubemark/throughput_override.yaml \
     --test-cmd-name=ClusterLoaderV2  2>&1 | ts | tee -a ${log_file}
