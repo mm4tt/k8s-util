9e40141881495318c5a743c0b65e98e2e6dfd283
Matt Matejczyk <mmat@google.com>
2019-07-23 18:15:58 +0200

    Fix a few things

--- experimental/golang-tests/config.sh
+++ experimental/golang-tests/config.sh
@@ -12,13 +12,6 @@ k8s_branch=golang_kubemark_932487c7440b05_no_patches
 # Some newer golang commits require some patches to build k8s, if k8s stops building, uncomment the line below.
 #k8s_branch=golang_kubemark_932487c7440b05
 
-# Golang commits to revert, seprated by commas.
-golang_revert_commits=""
-# If you're testing golang commit after the one below we need to revert
-# f1a8ca30fcaa91803c353999448f6f3a292f1db1 as it breaks k8s build.
-# So if golang_kubemark_932487c7440b05 branch didn't help, uncomment the line below.
-#golang_revert_commits="f1a8ca30fcaa91803c353999448f6f3a292f1db1"
-
 ###
 # Things that shouldn't be changed
 ###
