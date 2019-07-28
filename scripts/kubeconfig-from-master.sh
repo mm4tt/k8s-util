2a97604e2bf50b416482934bc12e6bf02d64a451
Mateusz Matejczyk <mmat@google.com>
2019-07-28 16:07:50 +0200

    Name kubeconfigs after master_name

--- scripts/kubeconfig-from-master.sh
+++ scripts/kubeconfig-from-master.sh
@@ -13,5 +13,5 @@ zone=${2?}
 master_name=${3?}
 
 gcloud beta compute --project "$project" ssh --zone "$zone" "$master_name"  --command "curl -s -H 'Metadata-Flavor: Google' http://metadata.google.internal/computeMetadata/v1/instance/attributes/kubeconfig > /tmp/kubeconfig"
-gcloud beta compute --project "$project" scp --zone "$zone" "$master_name":/tmp/kubeconfig /tmp/kubeconfig
-export KUBECONFIG=/tmp/kubeconfig
+gcloud beta compute --project "$project" scp --zone "$zone" "$master_name":/tmp/kubeconfig /tmp/kubeconfig_$master_name
+export KUBECONFIG=/tmp/kubeconfig_$master_name
