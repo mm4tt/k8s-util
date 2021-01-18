1dbc515ea3b8c3dee924122843f4e861a10ba8c8
Mateusz Matejczyk <mmat@google.com>
2021-01-18 10:31:30 +0100

    Disable prometheus server in cl2-compare-config

--- experimental/cl2-compare-config/run.sh
+++ experimental/cl2-compare-config/run.sh
@@ -26,19 +26,27 @@ mkdir cl2 && cd cl2
 git clone --single-branch --branch dump_cl2_steps https://github.com/mm4tt/perf-tests.git
 cd perf-tests/clusterloader2
 
+run_cl2() {
+  go run cmd/clusterloader.go \
+  --testconfig testing/load/config.yaml \
+  --report-dir="$tmp_dir" \
+  --provider=gce \
+  --kubeconfig=/dev/null \
+  --enable-prometheus-server=false
+}
+
 echo "Dumping config steps for master"
 cd "$tmp_dir/config/perf-tests"
 git checkout master
 cd "$tmp_dir/cl2/perf-tests/clusterloader2"
-go run cmd/clusterloader.go --testconfig testing/load/config.yaml --report-dir="$tmp_dir" --provider=gce --kubeconfig=/dev/null
+run_cl2
 cd "$tmp_dir" && mv steps.yaml master-steps.yaml
 
 echo "Dumping config steps for PR $pr_num"
 cd "$tmp_dir/config/perf-tests"
 git checkout "pr$pr_num"
 cd "$tmp_dir/cl2/perf-tests/clusterloader2"
-go run cmd/clusterloader.go --testconfig testing/load/config.yaml --report-dir="$tmp_dir" --provider=gce --kubeconfig=/dev/null
-cd "$tmp_dir" && mv steps.yaml "pr$pr_num-steps.yaml"
+run_cl2cd "$tmp_dir" && mv steps.yaml "pr$pr_num-steps.yaml"
 
 echo "Master config dumped in $tmp_dir/master-steps.yaml"
 echo "PR $pr_num config dumped in $tmp_dir/pr$pr_num-steps.yaml"
