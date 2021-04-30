306ef247ab431100d26b8daa830c2cf1323472d3
Matt Matejczyk <mmat@google.com>
2021-04-30 08:38:12 +0000

    Fix warning about no go.mod in set-common-envs

--- set-common-envs/set-common-envs.sh
+++ set-common-envs/set-common-envs.sh
@@ -3,8 +3,12 @@
 # This file exports env variables and should be run with: source .../set-common-envs.sh
 # Running it without source will span a new bash process that won't modify parent env variables.
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 preset_name=${1:-preset-e2e-scalability-common}
 commit=${2:-master}
 
-eval $(go run $DIR/print-common-envs.go --preset-name=$preset_name --commit=$commit)
+dir=`pwd`
+cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+eval $(go run print-common-envs.go --preset-name=$preset_name --commit=$commit)
+cd "$dir"
+
